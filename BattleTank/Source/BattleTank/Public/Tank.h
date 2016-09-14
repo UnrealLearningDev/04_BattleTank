cd1c7770ff2d96316f6d7e7d495823c23316476c
nathanimator <njglemb@gmail.com>
2016-09-13 22:33:52 -0500

    Created a launch speed variable

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#pragma once
+ #pragma once
 
 #include "TankAimingComponent.h"
 #include "GameFramework/Pawn.h"
@@ -17,8 +17,9 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Setup)
 	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
 
+	UPROPERTY(EditAnywhere, Category = Firing)
+	float LaunchSpeed = 100000;//TODO find sensible default
 
-protected:
 	UTankAimingComponent* TankAimingComponent = nullptr;
 
 private:
