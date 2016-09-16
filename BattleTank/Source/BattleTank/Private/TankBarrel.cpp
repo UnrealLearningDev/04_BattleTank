1af48ff44370b51cfe277c16ce9e476af087531f
nathanimator <njglemb@gmail.com>
2016-09-15 19:39:36 -0500

    mid lesson save before rebuilding project because of  vs update errors

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -5,9 +5,9 @@
 
 
 
-void UTankBarrel::Elevate(float DegreesPerSecond)
+void UTankBarrel::Elevate(float RelativeSpeed)
 {
 	//move the barrel the right amount this frame given a max elevation speed, and the frame time
-	UE_LOG(LogTemp, Warning, TEXT("Barrel Elevate() called at speed  %f "), DegreesPerSecond);
+	UE_LOG(LogTemp, Warning, TEXT("Barrel Elevate() called at speed  %f "), RelativeSpeed);
 }
 
