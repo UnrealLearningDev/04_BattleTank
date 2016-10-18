4adc6d76ca829612119ead8fe6d10becdc8bd7ad
nathanimator <njglemb@gmail.com>
2016-10-18 14:24:16 -0500

    commented out UE_LOG

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -31,7 +31,7 @@ void ATankPlayerController::AimTowardsCrosshair()
 
 	FVector HitLocation;// out parameter
 	bool bGotHitLocation = GetSightRayHitLocation(HitLocation);
-	UE_LOG(LogTemp, Warning, TEXT("bGotHitLocation: %i"), bGotHitLocation);
+	//UE_LOG(LogTemp, Warning, TEXT("bGotHitLocation: %i"), bGotHitLocation);
 	if (bGotHitLocation)//Has "side effect", is going to line trace
 	{
 		AimingComponent->AimAt(HitLocation);
